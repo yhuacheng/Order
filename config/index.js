1e5b24efb1988215367c18515ff5f888fbe6b174
Orange <635000035@qq.com>
2020-07-23 10:54:09 +0800

    页面及功能调整

--- config/index.js
+++ config/index.js
@@ -13,11 +13,11 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://203.195.212.239/',//设置你调用的接口域名和端口号 别忘了加http
-//      target: 'https://jsonplaceholder.typicode.com/posts/',//设置你调用的接口域名和端口号 别忘了加http
+        target: 'http://203.195.212.239/', //设置你调用的接口域名和端口号 别忘了加http
+        //      target: 'https://jsonplaceholder.typicode.com/posts/',//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '/'//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+          '^/api': '/' //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
         }
       }
     },
@@ -74,7 +74,7 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
